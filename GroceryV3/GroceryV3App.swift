4803b4b423e49ea08ea60beee7d7b0a5ac5194ae
jm <email@email.com>
2023-07-07 21:19:34 +0300

    Initial Commit

---
+++ GroceryV3/GroceryV3App.swift
@@ -0,0 +1,17 @@
+//
+//  GroceryV3App.swift
+//  GroceryV3
+//
+//  Created by JM Velarde on 7/7/23.
+//
+
+import SwiftUI
+
+@main
+struct GroceryV3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
